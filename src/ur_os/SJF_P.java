4bbf3f40f209e666e07f138be417153ea51f2df8
dav <124942043+merequetenguee@users.noreply.github.com>
2026-03-03 21:15:22 -0500

    Update SJF_P.java

--- src/ur_os/SJF_P.java
+++ src/ur_os/SJF_P.java
@@ -1,5 +1,4 @@
 package ur_os;
-
 import java.util.Comparator;
 
 public class SJF_P extends Scheduler {
@@ -35,53 +34,6 @@ public void newProcess(boolean cpuEmpty) {
         getNext(true);
         return;
     }
-
-    Process running = os.getProcessInCPU();
-
-    Process shortest = processes.stream()
-            .min(Comparator.comparingInt(Process::getRemainingTimeInCurrentBurst))
-            .orElse(null);
-
-    if (shortest == null)
-        return;
-
-    if (shortest.getRemainingTimeInCurrentBurst()
-            < running.getRemainingTimeInCurrentBurst()) {
-
-        
-        os.interrupt(
-            InterruptType.SCHEDULER_CPU_TO_RQ,
-            running
-        );
-
-      
-        removeProcess(shortest);
-
-        
-        os.interrupt(
-            InterruptType.SCHEDULER_RQ_TO_CPU,
-            shortest
-        );
-    }
-}
-
-    @Override
-    public void IOReturningProcess(boolean cpuEmpty) {
-        newProcess(cpuEmpty);
-    }
-    @Override
-public void update() {
-
-    boolean cpuEmpty = os.isCPUEmpty();
-
-    if (cpuEmpty) {
-        getNext(true);
-    } else {
-       
-        newProcess(false);
-    }
-}
-}
 
     Process running = os.getProcessInCPU();
 
